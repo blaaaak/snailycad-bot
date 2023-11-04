6d5a64e4f5be75163968580c454fd67596bc9202
blaaaak <103514165+blaaaak@users.noreply.github.com>
2023-11-04 15:43:27 +1100

    Update ready.ts

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -16,6 +16,6 @@ export default class ReadyEvent extends Event {
 
     new InteractionHandler(bot).loadInteractions();
 
-    bot.user?.setActivity("snailycad.org", { type: DJS.ActivityType.Watching });
+    bot.user?.setActivity("Snaily CAD", { type: DJS.ActivityType.Playing });
   }
 }
