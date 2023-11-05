64795fdaf5ec6cc0936fe9c1784e8e6445ddc6eb
blaaaak <103514165+blaaaak@users.noreply.github.com>
2023-11-05 15:35:08 +1100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Bot } from "./structures/Bot.js";
 
 const bot = new Bot();
 
-await bot.login(process.env["BOT_TOKEN"]);
+await bot.login(process.env.BOT_TOKEN);
 Sentry.init({
   dsn: "https://5152dff1b7334743b67ee49ae394aee6@o518232.ingest.sentry.io/6613141",
   tracesSampleRate: 1.0,
